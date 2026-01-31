2a8a8bbd953bc89af35c3864672344f69d3f657a
Shenbaga ganeshan S <115524824+ShenbagaganeshanS@users.noreply.github.com>
2026-01-31 11:46:05 +0530

    Clean up MongoDB connection code
    Removed comments and print statement related to MongoDB connection.

--- database/mongo.py
+++ database/mongo.py
@@ -1,10 +1,8 @@
 from pymongo import MongoClient
 from config import MONGO_URI, DB_NAME
 
-# Connect to MongoDB
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
-# Collection for expenses
 expense_collection = db["expenses"]
 print("MongoDB connected successfully")
